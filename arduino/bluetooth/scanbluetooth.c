b410350099d4ba2784b37a4c3b29f6f5636ea3ce
rodolfoitsz <rodolfo.jamr@gmail.com>
2016-12-11 01:56:24 -0600

    ...

--- arduino/bluetooth/scanbluetooth.c
+++ arduino/bluetooth/scanbluetooth.c
@@ -6,6 +6,31 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 
+
+/**
+ * Decode device class
+ */
+static void classinfo(uint8_t dev_class[3]) {
+	int flags = dev_class[2];
+	int major = dev_class[1];
+	int minor = dev_class[0] >> 2;
+
+	
+switch (major) {
+	case 1:
+		 printf(" %s", " type : computer ");
+		break;
+	case 2:
+		 printf(" %s", "type : phone ");
+		break;
+        default :
+
+               printf(" %s", "type: none ");
+	}
+
+}
+
+
 int main(int argc, char **argv)
 {
     inquiry_info *ii = NULL;
@@ -32,17 +57,26 @@ int main(int argc, char **argv)
 
  
     for (i = 0; i < num_rsp; i++) {
-        ba2str(&(ii+i)->bdaddr, addr);
-   
-       uint8_t  type [3];
-       int a =hci_read_class_of_dev(sock,&(ii+i)->bdaddr,0);
+      ba2str(&(ii+i)->bdaddr, addr);
+
+      classinfo(&(ii+i)->dev_class);
+
+
+       /*uint8_t type [3];
+	type = ii[i]->dev_class;*/
+
+      /* type[0]=&(ii+i)->dev_class[0];
+       type[1]=&(ii+i)->dev_class[1];
+       type[2]=&(ii+i)->dev_class[2];*/
 
         memset(name, 0, sizeof(name));
         if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), name, 0) < 0)    
         strcpy(name, "[unknown]");
         printf("%s  %s\n", addr, name);
-        printf("%d %d %d  %d \n",type[0],type[1],type[2],a);
+       //printf("%d %d %d the type\n",type[0],type[1],type[2]);
     }
+
+
 
     free( ii );
     close( sock );
